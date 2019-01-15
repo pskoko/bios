688f95d98931e99093c30a768dedd3a6135ea33f
MPozek <mislav.pozek@fer.hr>
2019-01-15 19:25:15 +0100

    adds function describing comments to the StringSuffixStructure.hpp

--- src/StringSuffixStructure.hpp
+++ src/StringSuffixStructure.hpp
@@ -20,8 +20,19 @@ private:
     const std::string& text;
 
 public:
+    /**
+    * Constructs a new StringSuffixStructure based on the provided string text reference.
+    */
     StringSuffixStructure(const std::string& text);
+
+    /**
+    * Returns the character literal reference at the text provided index 
+    */
     const char& operator[](const unsigned long index) const;
+
+    /**
+    * Returns the length of the text this suffix structure describes
+    */
     unsigned long getSize() const;
 };
 
