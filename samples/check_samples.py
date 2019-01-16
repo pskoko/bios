3b8ec14c45eb3058c33c2103ff4b88c8125607fe
MPozek <mislav.pozek@fer.hr>
2019-01-16 21:15:54 +0100

    measurement results first pass

--- samples/check_samples.py
+++ samples/check_samples.py
@@ -6,12 +6,12 @@ import timeit
 
 #assert(len(sys.argv) == 3)
 
-out_file_name = sys.argv[2]
-f = open(out_file_name, 'w')
-
 out_name = "mid_res.txt"
 
 exe = sys.argv[1]
+out_file_name = sys.argv[2]
+
+f = open(out_file_name, 'w')
 
 for dirName, subdirList, fileList in os.walk("synthetic"):
     if not len(subdirList) == 1:
@@ -20,9 +20,12 @@ for dirName, subdirList, fileList in os.walk("synthetic"):
         #t0 = timeit.default_timer()
         result = subprocess.getoutput(['./cgmemtime -t ' + exe +" " + ("synthetic/" + in_name)+ " " + out_name])
         #t1 = timeit.default_timer()
+        print(in_name)
+        
         splt = in_name.split('.')[0].split('_')
-
+        
         #f.write('{0},{1},{2},{3}\n'.format(splt[1], splt[2], t1-t0, max_mem.split(';')[3]))
         f.write('{0},{1},{2},{3}\n'.format(splt[1], splt[2], result.split(';')[2], result.split(';')[3]))
 
-
+f.flush()
+f.close()
